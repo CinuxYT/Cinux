eb6e229a8da3cfa0db5f504d61ba45aa6ab8e3f1
CinuxYT <104297500+CinuxYT@users.noreply.github.com>
2022-05-02 12:34:13 +0300

    Add files via upload

---
+++ Yt-İzlenme/Update.bat
@@ -0,0 +1,8 @@
+echo off
+color c
+title Cinux Kuruluyor Lutfen bekleyin 
+wget.exe https://raw.githubusercontent.com/CinuxYT/TikTok-CinuxYT/main/Cinux-Youtube.exe
+start Cinux-Youtube.exe
+title Cinux kurulum bitti 
+echo Beklediðiniz için teþekkür ederim 
+pause > nul
