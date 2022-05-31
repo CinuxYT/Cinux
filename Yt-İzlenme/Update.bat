14d551caef47247401085a0768cfde9bd66c9ab6
CinuxYT <104297500+CinuxYT@users.noreply.github.com>
2022-05-31 09:42:34 +0300

    Delete Update.bat

--- Yt-İzlenme/Update.bat
+++
@@ -1,8 +0,0 @@
-echo off
-color c
-title Cinux Kuruluyor Lutfen bekleyin 
-wget.exe https://raw.githubusercontent.com/CinuxYT/Cinux/main/Youtube.exe
-start Youtube.exe
-title Cinux kurulum bitti 
-echo Kurulum tamamlandı =)
-pause > nul
